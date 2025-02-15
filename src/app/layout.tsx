3389609102dad30c3d68b3931a276a3df28b731a
sina <sinaa.afshar@gmail.com>
2025-02-15 15:46:24 +0330

    add nextjs project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Check Crypto Prices Online",
+  description: "if you want to check most popular crypto prices you can be with us",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
